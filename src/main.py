31880f3fca9ecb388d3ee90b539a5b5c0d30d985
Alexander Lundahl <lual23jp@student.ju.se>
2025-03-26 14:12:12 +0100

    Added classes folder with functioning fps_counter

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
 import pygame as pg
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+
+from classes.fps_counter import fps_counter # Import fps_counter class from classes/fps_counter.py
 
 pg.init()
 
@@ -11,6 +16,8 @@ pg.display.set_caption("Particle Sandbox")
 
 clock = pg.time.Clock()
 
+fps_counter = fps_counter(window, pg.font.Font(None, 30), clock, (255, 255, 255), (60, 25)) # fps_counter(window, font, clock, color, pos)
+
 def main():
     while True:
         for event in pg.event.get():
@@ -20,8 +27,15 @@ def main():
 
         window.fill((0, 0, 0))
 
+
+        fps_counter.update() # Update the fps_counter
+        fps_counter.render() # Render the fps_counter
+
+
         pg.display.update()
-        clock.tick(FPS)
+        clock.tick(FPS) # clock ticks at the specified FPS
+
+        
 
 
 
