e9164f2c41c04ba9d18d1c69027a2732c525bc4a
Simon Lorentzon <losi23mj@student.ju.se>
2025-03-26 13:22:42 +0100

    Added gameloop with simple window

--- src/main.py
+++ src/main.py
@@ -1,7 +1,27 @@
 import pygame as pg
 
+pg.init()
+
+WINDOW_WIDTH = 1000
+WINDOW_HEIGHT = 800
+FPS = 120
+
+window = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pg.display.set_caption("Particle Sandbox")
+
+clock = pg.time.Clock()
+
 def main():
-    print("hello world")
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                quit()
+
+        window.fill((0, 0, 0))
+
+        pg.display.update()
+        clock.tick(FPS)
 
 
 
