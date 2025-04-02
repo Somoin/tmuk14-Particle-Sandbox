82412137f35bc906a9ec65f8b0db48fa96328466
Edrin Zahiri <zaed23kv@student.ju.se>
2025-04-02 17:22:36 +0200

    Added different shades of sand

--- src/classes/particles/sandParticle.py
+++ src/classes/particles/sandParticle.py
@@ -1,9 +1,18 @@
 from particle import Particle
+import random as rd
+
+sand_colors = [
+    (246,215,176),
+    (242,210,169),
+    (236,204,162),
+    (231,196,150),
+    (225,191,146)
+]
 
 
 class SandParticle(Particle):
     def __init__(self, grid, x, y):
-        super().__init__("sand", (194, 178, 128), grid, x ,y)
+        super().__init__("sand", rd.choice(sand_colors), grid, x ,y)
     
     def update(self, grid, x, y):
         if y == self.grid.rows-1:
