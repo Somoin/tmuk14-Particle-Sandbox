0d2c287b71ed99e9af608260c49f06119a75a008
Edrin Zahiri <zaed23kv@student.ju.se>
2025-04-03 15:44:27 +0200

    Added text that displays the current particle

--- src/main.py
+++ src/main.py
@@ -17,7 +17,7 @@ from enum import Enum
 class ParticleType(Enum):
     SAND = 1
     WATER = 2
-    CEMENT = 3
+    CONCRETE = 3
     AIR = 4
     SMOKE = 5
     WOOD = 6 
@@ -57,13 +57,21 @@ wood_button = Button("src/images/woodbutton.png", 900, 550, "src/images/woodbutt
 gunpowder_button = Button("src/images/gunpowderbutton.png", 100, 650, "src/images/gunpowderbutton_hover.png")
 
 
+text_pos_x = 650
+text_pos_y = 15
+def display_current_particle(display_text, pos_x, pos_y):
+    text = pg.font.Font(None, 30).render("Current Particle: " + display_text, True, (255, 255, 255))
+    text_rect = text.get_rect(topleft=(pos_x, pos_y))
+    window.blit(text, text_rect)
+
+
 def particle_input(particle_type, cursor_type, grid, mouseX, mouseY):
     if particle_type == ParticleType.SAND:
         simulation.add_particle(SandParticle(simulation.grid, mouseX, mouseY), mouseX, mouseY)
     # elif particle_type == ParticleType.WATER:
     #     print("Water Particle Added")
             #simulation.add_particle(WaterParticle(), pg.mouse.get_pos()[0]//cell_size, pg.mouse.get_pos()[1]//cell_size)
-    elif particle_type == ParticleType.CEMENT:
+    elif particle_type == ParticleType.CONCRETE:
         simulation.add_particle(ConcreteParticle(simulation.grid, mouseX, mouseY), mouseX, mouseY)
     elif particle_type == ParticleType.SMOKE:
         simulation.add_particle(SmokeParticle(simulation.grid, mouseX, mouseY, lifetime=10), mouseX, mouseY)
@@ -106,7 +114,7 @@ def main():
                     particle_type = ParticleType.WATER
                 if concrete_button.check_press(pg.mouse.get_pos()):
                     print("Concrete Button Pressed")
-                    particle_type = ParticleType.CEMENT
+                    particle_type = ParticleType.CONCRETE
                 if fire_button.check_press(pg.mouse.get_pos()):
                     print("Fire Button Pressed")
                     particle_type = ParticleType.FIRE
@@ -118,6 +126,7 @@ def main():
                     particle_type = ParticleType.GUNPOWDER
                 print(particle_type.name)
 
+
             # Debug Keyboard input
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_1:
@@ -127,7 +136,7 @@ def main():
                     particle_type = ParticleType.WATER
                     print("Water Key Pressed")
                 if event.key == pg.K_3:
-                    particle_type = ParticleType.CEMENT
+                    particle_type = ParticleType.CONCRETE
                     print("Cement Key Pressed")
                 if event.key == pg.K_4:
                     particle_type = ParticleType.SMOKE
@@ -141,6 +150,8 @@ def main():
                 if event.key == pg.K_7:
                         particle_type = ParticleType.GUNPOWDER
                         print("Gunpowder Key Pressed")
+
+                display_current_particle(particle_type.name, text_pos_x, text_pos_y)
              
                 # Changing cursor type
                 if event.key == pg.K_q:
@@ -159,7 +170,7 @@ def main():
                 simulation.add_particle(SandParticle(simulation.grid, mouseX, mouseY), mouseX, mouseY)
             elif particle_type == ParticleType.WATER:
                 simulation.add_particle(WaterParticle(simulation.grid, mouseX, mouseY), mouseX, mouseY)
-            elif particle_type == ParticleType.CEMENT:
+            elif particle_type == ParticleType.CONCRETE:
                 simulation.add_particle(ConcreteParticle(simulation.grid, mouseX, mouseY), mouseX, mouseY)
             elif particle_type == ParticleType.SMOKE:
                 simulation.add_particle(SmokeParticle(simulation.grid, mouseX, mouseY, lifetime=10), mouseX, mouseY)
@@ -189,11 +200,7 @@ def main():
                         mouseX = pg.mouse.get_pos()[0]//cell_size - cursor_block_height//2 + i
                         mouseY = pg.mouse.get_pos()[1]//cell_size - cursor_block_width//2 + j
                         simulation.remove_particle(mouseX+i, mouseY+j)
-                
-
-              
-
-        
+    
 
         window.fill((0, 0, 0))
         sand_button.draw(window)
@@ -212,7 +219,7 @@ def main():
         fps_counter.update() # Update the fps_counter
         fps_counter.render() # Render the fps_counter
 
-
+        display_current_particle(particle_type.name, text_pos_x, text_pos_y)
         pg.display.update()
         clock.tick(FPS) # clock ticks at the specified FPS
 
