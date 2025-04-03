236e92c066d2f700ede6808be12bc2d150f116a7
Simon Lorentzon <losi23mj@student.ju.se>
2025-04-03 11:27:31 +0200

    Added flammability multiplier for flammable particles

--- src/classes/particles/gunpowderParticle.py
+++ src/classes/particles/gunpowderParticle.py
@@ -10,6 +10,7 @@ class GunpowderParticle(Particle):
     def __init__(self, grid, x, y):
         super().__init__("gunpowder", rd.choice(colors), grid, x ,y)
         self.flammable = True
+        self.flammability = 5 # Multiplier of flammability
     
     def update(self, grid, x, y):
         if y == self.grid.rows-1: # out of bounds bottom
--- src/classes/particles/woodParticle.py
+++ src/classes/particles/woodParticle.py
@@ -11,6 +11,7 @@ class WoodParticle(Particle):
     def __init__(self, grid, x, y):
         super().__init__("wood", rd.choice(colors), grid, x ,y)
         self.flammable = True # Wood is flammable
+        self.flammability = 1
 
 
     def update(self, grid, x, y):
