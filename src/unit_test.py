316b5d3ffa19fa77bc2a8330617c47563bbf2cdd
Alexander Lundahl <lual23jp@student.ju.se>
2025-04-16 12:22:01 +0200

    Added unit test for when you hover a button

--- src/unit_test.py
+++ src/unit_test.py
@@ -1,8 +1,27 @@
 import pytest
+import pygame as pg
+from configparser import ConfigParser
 
-def func(x):
-    return x + 1
+from classes.button import Button
+
+
+pg.init()
+config_object = ConfigParser()
+
+config_object.read("config.ini")
+cell_size = config_object.getint("CONFIG", "cell_size")
+window_width = config_object.getint("CONFIG", "window_width")
+window_height = config_object.getint("CONFIG", "window_height")
+FPS = config_object.getint("CONFIG", "FPS")
+window = pg.display.set_mode((window_width, window_height))
+
+pg.display.set_caption("Particle Sandbox")
+
+
+def button_check_hover(x):
+    button = Button("images/sandbutton.png", 0, 0, "images/sandbutton_hover.png", 100, 100)
+    return button.check_hover((x, x))
 
 
 def test_answer():
-    assert func(3) == 5
+    assert button_check_hover(10) == True
